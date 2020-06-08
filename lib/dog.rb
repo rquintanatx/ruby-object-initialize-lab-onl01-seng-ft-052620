b12e2a105b5aea8a2ea2c45328ae66338a77a840
Robert Quintana <robertquintanatx@gmail.com>
2020-06-08 06:34:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,7 @@ class Dog
 def initialize(name, breed)
     @name = name
     
-    if breed = nil 
-      @breed = "Mutt"
-    else
-      @breed = breed
-    end 
+    @breed = "Mutt"
 end
   
 end 
